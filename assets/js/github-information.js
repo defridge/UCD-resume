ca2fa55d22fd58ad049561533a4d682bec2f0e1e
Luke Masterson <lukemasterson@Lukes-MacBook-Air.local>
2024-03-29 21:52:20 +0000

    started adding code for github API

---
+++ assets/js/github-information.js
@@ -0,0 +1,27 @@
+function fetchGitHubInformation(event) {
+
+    var username = $("#gh-username").val();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    }
+
+    $("#gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
+
+    $.when(
+        $getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        } else {
+            console.log(errorResponse);
+            $("#gh-user-data").html(
+                `<h2>Error: ${errorResponse.responseJSON.message</h2>}`
+            )
+        }
+    )
+}
